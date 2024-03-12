871f6cba591f5eeeebe0ab8cdbba6e3fc8b656ab
rphsantoss <raapphaaeell@gmail.com>
2024-03-12 16:55:19 -0300

    creating account

--- index.js
+++ index.js
@@ -17,7 +17,52 @@ function operation() {
     ]).then(answer => {
 
         const action = answer['action']
-        console.log(action)
+        
+        if(action === 'Criar conta') {
+            createAccount()
+        }
 
     }).catch(err => console.log(err))
+}
+
+//create an account
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Obrigado por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções a seguir:'))
+
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual será o nome da sua conta? '
+
+        }    
+    ]).then(answer => {
+
+        const accountName = answer['accountName']
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Este nome de conta já existe, escolha outro!'))
+
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{balance": 0}', function(err){
+            console.log(err)
+        })
+
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+        operation()
+
+    }).catch((err) => console.log(err))
 }
