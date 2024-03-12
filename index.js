e828b7886671325fdf7e0eefb919c7b85bb16a38
rphsantoss <raapphaaeell@gmail.com>
2024-03-12 16:26:20 -0300

    system initialization

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const inquirer = require('inquirer')
 const chalk = require('chalk')
 
 const fs = require('fs')
+
+operation()
+
+function operation() {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: ['Criar conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair'],
+        },
+    ]).then(answer => {
+
+        const action = answer['action']
+        console.log(action)
+
+    }).catch(err => console.log(err))
+}
